fee0a6c761aa5ddc6cd2c1b9f72d77759ef6f4e2
Marconi <marconi4305@gmail.com>
2024-10-31 17:20:38 -0300

    agrego primeng y notificaciones de inicio de sesion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule),
     provideFirebaseApp(() => initializeApp(
       {
         "projectId":"task-8ffac",
--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { LoginService } from './login.service';
+import {ToastModule} from 'primeng/toast'
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'login.component.html',
     standalone: true,
-    imports:[RouterLink, RouterLinkActive, ReactiveFormsModule]
+    imports:[RouterLink, RouterLinkActive, ReactiveFormsModule,ToastModule, ButtonModule ],
+    providers: [MessageService]
 })
 
 export class LoginComponent implements OnInit {
@@ -15,7 +19,7 @@ export class LoginComponent implements OnInit {
     private readonly service = inject(LoginService);
     private readonly router = inject(Router);
 
-    constructor() { }
+    constructor(private messageService: MessageService) { }
 
     protected readonly form = this.fb.group({
         nombreUsuario: [],
@@ -26,7 +30,22 @@ export class LoginComponent implements OnInit {
 
     loginGoogle(){
         this.service.loginWithGoogle()
-            .then(() => this.router.navigate(['/home']))
-            .catch(error => console.error('Login error', error));
+            .then(() => {
+                this.messageService.add({
+                    severity:'success', 
+                    summary:'Inicio de Sessión', 
+                    detail:'Inicio de sesión exitoso.'});
+
+                setTimeout(() => {
+                        this.router.navigate(['/home']);
+                    }, 1000);
+            })
+            .catch(() => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Error de Inicio de Sesión',
+                    detail: 'Hubo un problema al iniciar sesión. Inténtalo de nuevo.'
+                });
+            });
     };
 }
