956f80f627371105cd735a2026c08d98a610deea
Marconi <marconi4305@gmail.com>
2024-11-01 19:22:47 -0300

    agrego config si no estas logueado no podes entrar al home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { RegisterComponent } from './features/register/register.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/login' },
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent,
+        ...canActivate(() => redirectUnauthorizedTo(['/login']))
+     },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
