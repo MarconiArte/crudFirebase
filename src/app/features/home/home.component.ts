b7a862a675cf2f3ea9ec8279b7cc7722734bef96
marcos.niz <marcos.niz@corsisa.com.ar>
2024-11-01 12:27:54 -0300

    agrego navbar con nombre y foto de usuario al home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,26 +1,49 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { User } from '@angular/fire/auth';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageService } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
+import { Router } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.component.html',
-    standalone:true,
+    standalone: true,
+    imports: [ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule, AvatarModule, ToastModule],
+    providers: [MessageService]
 })
 
 export class HomeComponent implements OnInit {
     private readonly service = inject(HomeService);
+    private readonly router = inject(Router)
 
     protected user: User | null = null;
-    constructor() { }
+    constructor(private messageService: MessageService) { }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.service.user$.subscribe(user => {
             this.user = user;
         });
     }
 
-    logout(){
-        this.service.logout();
+    logout() {
+        this.service.logout()
+            .then(() => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Cerrar sesión',
+                    detail: 'Ha cerrado sesión con éxito.'
+                });
+                setTimeout(() => {
+                    this.router.navigate(['/login']);
+                }, 1000);
+            })
+            .catch(error => console.log(error)
+            );
     }
 }
--- src/app/features/home/home.service.ts
+++ src/app/features/home/home.service.ts
@@ -1,23 +1,17 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Auth, authState, signOut, User } from '@angular/fire/auth';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HomeService {
-    private readonly router = inject(Router);
 
     user$: Observable<User | null>;
-    
-    constructor(private auth: Auth) { 
+
+    constructor(private auth: Auth) {
         this.user$ = authState(this.auth);
     }
-    
-    logout(){
-        return signOut(this.auth)
-            .then(() => {
-                return this.router.navigate(['/login']);
-            })
-            .catch(error => console.error(error));
+
+    logout() {
+        return signOut(this.auth);
     }
 }
